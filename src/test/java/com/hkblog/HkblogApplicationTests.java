b76d9496115181cb64b535995d3c5f61f78846e0
HK-Kim <sswwx1@naver.com>
2023-01-03 10:24:17 +0900

    first commit

---
+++ src/test/java/com/hkblog/HkblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hkblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HkblogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
