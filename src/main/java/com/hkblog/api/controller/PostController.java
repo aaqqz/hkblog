6e3b4b7a0e50442f7e491f57275c8884c0be26d9
HK-Kim <sswwx1@naver.com>
2023-01-03 16:28:17 +0900

    post json data Test

--- src/main/java/com/hkblog/api/controller/PostController.java
+++ src/main/java/com/hkblog/api/controller/PostController.java
@@ -1,10 +1,24 @@
 package com.hkblog.api.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.hkblog.api.request.PostCreate;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Map;
+
+@Slf4j
 @RestController
 public class PostController {
+
+    // Http Method
+    // GET, POST, PUT, PATCH, DELETE, OPTIONS, HEAD, TRACE, CONNECT
+    // 글 등록
+    // POST Method
+    @PostMapping("/posts")
+    public String post(@RequestBody PostCreate params) {
+        log.info("params={}", params.toString());
+        return "Hello World";
+    }
 
     @GetMapping("/posts")
     public String get() {
--- src/test/java/com/hkblog/api/controller/PostControllerTest.java
+++ src/test/java/com/hkblog/api/controller/PostControllerTest.java
@@ -4,10 +4,12 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -16,6 +18,20 @@ class PostControllerTest  {
 
     @Autowired
     MockMvc mockMvc;
+
+    @Test
+    @DisplayName("/posts 요청시 Hello World를 출력한다.")
+    void postTest() throws Exception {
+
+        // expected
+        mockMvc.perform(post("/posts")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"title\": \"제목입니다.\", \"content\": \"내용입니다.\"}")
+                )
+                .andExpect(status().isOk())
+                .andExpect(content().string("Hello World"))
+                .andDo(print());
+    }
 
     @Test
     @DisplayName("/posts 요청시 Hello World를 출력한다.")
